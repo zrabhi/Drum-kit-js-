78693450fb7d0440eabc9e7bf91c65da9bfe74e1
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-05-31 17:36:58 +0100

    adding event to keys

--- Javascript-Drum-Kit/script.js
+++ Javascript-Drum-Kit/script.js
@@ -1,3 +1,28 @@
-//?
+
+
+// function PlayEvent(e)
+// {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+
+//     key.classList.add('playing');
+//     // audio.currentTime = 0;
+//     audio.play();
+    
+// };ds
+window.addEventListener('keydown', (e) =>
+{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    // audio.currentTime = 0;
+    audio.play();
+    // key.classList.remove('playinga');
+    // key.classList.add('trans');
+});
+
 
 

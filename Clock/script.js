d6ac6c3dd713e104b9f4d708efe0a254896092c5
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-02 10:31:43 +0100

    simple bugs to finish the clock

--- Clock/script.js
+++ Clock/script.js
@@ -1,5 +1,6 @@
 
 const SecondHand = document.querySelector('.sec-hand');
+const MinuteHand = document.querySelector('.min-hand');
 
 function SetDate()
 {
@@ -7,9 +8,13 @@ function SetDate()
     const Second = date.getSeconds();
     const Minute = date.getMinutes();
     const Hour = date.getHours();
-    const SecondRotationDeg = ((Second / 60) * 360) + 90;
+    const SecRotationDeg = ((Second / 60) * 360) + 90;
+    const MinRotationDeg = ((Minute / 60 ) * 360) + 90;
+    console.log("degree for seconds", SecRotationDeg);
+    // const HourRotationDef =  ((Hour / 60 ) + 360) + 90;
     // SecondHand.style.transform = 
-    SecondHand.style.transform = `rotate(${SecondRotationDeg}deg)`;
+    SecondHand.style.transform = `rotate(${SecRotationDeg}deg)`;
+    MinuteHand.style.transform = `rotate(${MinRotationDeg}deg)`;
     console.log("Second ==>", Second);
     console.log("Minute", Minute); 
     console.log("Hour", Hour);

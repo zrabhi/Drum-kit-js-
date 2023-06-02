325e5e0ded12d6d68f55cd1b8a6089eab78157fe
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-02 11:29:22 +0100

    one minor step to finish

--- Clock/script.js
+++ Clock/script.js
@@ -1,7 +1,7 @@
 
 const SecondHand = document.querySelector('.sec-hand');
 const MinuteHand = document.querySelector('.min-hand');
-
+const HourHand = document.querySelector('.hour-hand')
 function SetDate()
 {
     const date = new Date();
@@ -10,11 +10,13 @@ function SetDate()
     const Hour = date.getHours();
     const SecRotationDeg = ((Second / 60) * 360) + 90;
     const MinRotationDeg = ((Minute / 60 ) * 360) + 90;
-    console.log("degree for seconds", SecRotationDeg);
+    const HourRotationDeg = ((Minute / 13) * 360) + 90;
+    console.log("degree for Hour", HourRotationDeg);
     // const HourRotationDef =  ((Hour / 60 ) + 360) + 90;
     // SecondHand.style.transform = 
     SecondHand.style.transform = `rotate(${SecRotationDeg}deg)`;
     MinuteHand.style.transform = `rotate(${MinRotationDeg}deg)`;
+    HourHand.style.transform = `rotate(${HourRotationDeg}deg)`;
     console.log("Second ==>", Second);
     console.log("Minute", Minute); 
     console.log("Hour", Hour);

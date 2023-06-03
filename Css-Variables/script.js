5c47c556a1dda12b10cb98e541f1334e18db5e50
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-03 16:27:58 +0100

    simple bug left to finish

--- Css-Variables/script.js
+++ Css-Variables/script.js
@@ -1,7 +1,15 @@
 const Inputs = document.querySelectorAll('.controls input');
+let baseEl = document.getElementById('base');
+const  baseColor = document.documentElement;
+console.log(baseColor);
 
-Inputs.forEach(input => input.addEventListener( 'change' || 'mousemove', () =>
+
+const Color = baseColor.style.getPropertyValue("--base");
+console.log(Color);
+
+Inputs.forEach(input => input.addEventListener( 'mousemove', () =>
 {
    document.documentElement.style.setProperty(`--${input.name}`,
-         (input.value + (input.dataset.sizing || '' )));
+      (input.value + (input.dataset.sizing || '' )));
+   console.log(input.value + (input.dataset.sizing || '' ));
 }))

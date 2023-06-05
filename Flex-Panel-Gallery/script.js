7fd64c9bd8db2ba69bf1eed521ee4f00eb76cf36
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-05 23:47:00 +0100

    finishing project

---
+++ Flex-Panel-Gallery/script.js
@@ -0,0 +1,19 @@
+const Panels = document.querySelectorAll('.panel');
+
+console.log(Panels); 
+
+function ToggleActive(e)
+{
+    this.classList.toggle('open');
+}
+
+function EventHandler(e)
+{
+    if (e.propertyName === 'flex' )
+        this.classList.toogle('open-active')
+    console.log(e.propertyName);
+    // console.log(e.propertyName.includes('flex'));
+}
+
+Panels.forEach(element => element.addEventListener('click', EventHandler)) ;
+Panels.forEach(element => element.addEventListener('click', ToggleActive)) ;
